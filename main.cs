8a505d2650bbcf04b89dd6d3cc11210947064825
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-03-28 17:08:45 +0000

    fixx

--- main.cs
+++ main.cs
@@ -3,17 +3,16 @@
 //
 // This program does basic math
 
-using System; 
+using System;
 
-public class Program 
+class Program 
 {
+  public static void Main (string[] args) 
+  {
+    Console.WriteLine ("The dimensions of a rectangle is 2 cm and 5 cm.");
+    Console.WriteLine ("The area is: " + (2 * 5) + " cm²");
+    Console.WriteLine ("The perimeter is: " + (2 * (2 + 5)) + " cm");
 
-    public static void Main(string[] args) 
-    {
-        Console.WriteLine("The dimention of a rectangle are: 5 cm and 3 cm ");
-        Console.WriteLine("The area is " + (5 * 3) + " cm².");
-        Console.WriteLine("The perimeter is: " + (2 * (5+3)) + " cm.");
-
-        Console.WriteLine("\nDone.");
-    }
+    Console.WriteLine ("\nDone.");
+  }
 }
