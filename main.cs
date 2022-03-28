44eba8ee9bc646ea38a647c447a66909b15b9f07
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-03-28 16:56:33 +0000

    done

--- main.cs
+++ main.cs
@@ -1,7 +1,19 @@
-using System;
+// Created by: Jaejun
+// Created on: Mar 2022
+//
+// This program does basic math
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+using System; 
+
+public class Program 
+{
+
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine("The dimention of a rectangle are: 5 cm and 3 cm ");
+        Console.WriteLine("The area is " + (5 * 3) + " cm².");
+        Console.WriteLine("The perimeter is: " + (2 * (5+3)) + " cm.");
+
+        Console.WriteLine("\nDone.");
+    }
 }
